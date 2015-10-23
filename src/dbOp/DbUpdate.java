219b46a3bab7a5e86611ca4a0dcffcf4248996aa
BLF2 <blf20822@126.com>
2015-10-23 23:27:14 +0800

    update

--- src/dbOp/DbUpdate.java
+++ src/dbOp/DbUpdate.java
@@ -1,5 +1,15 @@
 package dbOp;
 
-public class DbUpdate {
+import login.*;
+import java.sql.*;
 
+public class DbUpdate {
+	private Statement state;
+	public DbUpdate() throws SQLException{
+		state = DbDriver.createStatement();
+	}
+	public int DbUpdateLoginInfo(LoginInfo li) throws SQLException{
+		String sql = "update LoginInfo set LoginName='"+li.getLoginName()+"',LoginPswd='"+li.getLoginPswd()+"'+LoginNum="+li.getLoginNum()+" where LoginId="+li.getLoginId()+";";
+		return state.executeUpdate(sql);
+	}
 }
