555be82e3b7fdf56bd8f82571739724a066c9c83
BLF2 <blf20822@126.com>
2015-10-24 21:39:48 +0800

    update servlet

---
+++ src/ServletDo/ServletLoginOrRegister.java
@@ -0,0 +1,35 @@
+package ServletDo;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.rmi.server.Dispatcher;
+
+
+public class ServletLoginOrRegister extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Reponse(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Reponse(request,response);
+	}
+	private void Reponse(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cmd = request.getParameter("Submit");
+		if(cmd.equals("login")){
+			response.sendRedirect("");
+		}else if(cmd.equals("register")){
+			response.sendRedirect("");
+		}else if(cmd.equals("Login")){
+			
+		}else if(cmd.equals("Register")){
+			
+		}
+	}
+}
